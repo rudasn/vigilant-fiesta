08f749c633a6dc2ee828fd2790d44f46a3bee4ad
Nicolas R <rudasn@gmail.com>
2017-12-06 10:55:12 +0200

    code comments

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,3 +1,25 @@
+/**
+ * EventDispatcher
+ *
+ * The purpose of the event dispatcher is to apply a series of actions
+ * captured from the EventRecorder on the specified context.
+ *
+ * @example Dispatch a series of `mousemove` and `click` actions.
+ *  const player = new EventDispatcher({
+ *      trigger: (event, context) => {
+ *          console.log(`Triggered "${ event.type }"`)
+ *      }
+ *  })
+ *  player.play(recorder.queue)
+ *
+ * @constructor
+ * @param {object} props
+ * @param {function} props.trigger - the function to call when triggering
+ *      an event. Accents the extracted `event` and the provided `context`.
+ * @param {object} [props.context] - The context of the event recorder.
+ *      Listeners will be attached on this object.
+ *      Can be a DOM `node`, `document`, or `window` (default).
+ */
 class EventDispatcher {
     constructor({ trigger, context=window }) {
         this.props = {
@@ -8,6 +30,14 @@ class EventDispatcher {
         this.currentFrame = 0
         this.currentActions = []
     }
+    /**
+     * Dispatch the event at `frame` from the `actions` array.
+     * Provides a time delay considering the time delta between the
+     * previous event in the `actions` and the current event.
+     *
+     * @param {object[]} - An array of captured events
+     * @param {number} [frame] - The index of the event we want to dispatch
+     */
     dispatch(actions=this.currentActions, frame=this.currentFrame) {
         const { trigger, context } = this.props
         const { isPaused } = this
@@ -27,6 +57,8 @@ class EventDispatcher {
         console.log(`dispatching #${ frame } (${ next.type }) in ${ timeout }ms`)
 
         setTimeout(() => {
+            if (this.isPaused) { return }
+
             requestAnimationFrame(() => {
                 try {
                     trigger(next, context)
@@ -39,17 +71,31 @@ class EventDispatcher {
             })
         }, timeout)
     }
+    /**
+     * Apply a set of events.
+     *
+     * @param {object[]} - An array of captured events
+     * @param {number} [frame] - The index of the event we want to begin with
+     */
     play(actions, frame) {
         this.isPaused = false
         this.dispatch(actions, frame)
         console.log('play', frame)
+        return this
     }
+    /**
+     * Stop the playback of events.
+     */
     stop() {
         this.isPaused = true
         console.log('stop')
+        return this
     }
+    /**
+     * Restart playback of events, starting from the first one.
+     */
     restart() {
-        this.play(this.currentActions, 0)
+        return this.play(this.currentActions, 0)
     }
 }
 
--- src/recorder.js
+++ src/recorder.js
@@ -1,3 +1,28 @@
+/**
+ * EventRecorder
+ *
+ * The purpose of the recorder is to add event listeners
+ * and extract useful information from captured events.
+ *
+ * @example Capture `mousemove` and `click` events from the current window.
+ *  const recorder = new EventRecorder([ 'mousemove', 'click' ], {
+ *      // extract the type of the captured event.
+ *      extract: (event, context) => ({ type: event.type }),
+ *      context: window,
+ *  })
+ *  recorder.start()
+ *  recorder.queue // => holds the captured events
+ *
+ * @constructor
+ * @param {string[]} events - An array of event types to listen to.
+ * @param {object} props
+ * @param {function} props.extract - The function to extract data & meta-data
+ *      from the captured events. Accepts the captured `event` and the
+ *      provided `context` and optionally returns an `object`.
+ * @param {object} [props.context] - The context of the event recorder.
+ *      Listeners will be attached on this object.
+ *      Can be a DOM `node`, `document`, or `window` (default).
+ */
 class EventRecorder {
     constructor(events, { extract, context=window }) {
         this.events = events
@@ -8,22 +33,35 @@ class EventRecorder {
             context,
         }
     }
+    /**
+     * Add a listener for each event on the context provided on initialization.
+     */
     start() {
         const { events, capture } = this
         const { context } = this.props
 
         events.map(name =>
-            context.addEventListener(name, capture)
+            context.addEventListener(name, capture, true)
         )
+        return this
     }
+    /**
+     * Remove all event listeners.
+     */
     stop() {
         const { events, capture } = this
         const { context } = this.props
 
         events.map(name =>
-            context.removeEventListener(name, capture)
+            context.removeEventListener(name, capture, true)
         )
+        return this
     }
+    /**
+     * Callback for event listeners.
+     * If the `extract` function provided on initialization returns a non-falsey
+     * value the value is appended on the instance's `queue` property.
+     */
     capture(event) {
         const { extract, context } = this.props
         const { queue } = this
@@ -31,7 +69,7 @@ class EventRecorder {
         const action = extract(event, context)
 
         if (action) {
-            console.log('capture', action.type, action)
+            console.log('captured', action.type, action)
             queue.push(action)
         }
     }
